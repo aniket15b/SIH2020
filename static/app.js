840610137d3bf9f3691dbe7d32a7f40b86cebc38
Neelansh Mathur <neelansh15@outlook.com>
2020-01-30 17:51:17 +0530

    RitikAddedTHIS

--- static/app.js
+++ static/app.js
@@ -21,7 +21,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
-  deferredPrompt = e;
+  this.deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = 'block';
 
