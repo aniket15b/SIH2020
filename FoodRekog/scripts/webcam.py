72bae9d7254d998daa015b75bd7335996da1e0f4
Techno-Disaster <nitinnirve@gmail.com>
2020-01-30 10:08:42 +0530

    add again

---
+++ FoodRekog/scripts/webcam.py
@@ -0,0 +1,195 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import cv2 as cv2
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+import time
+
+# patch tf1 into `utils.ops`
+utils_ops.tf = tf.compat.v1
+
+# Patch the location of gfile
+tf.gfile = tf.io.gfile
+
+# def load_model(model_name):
+#   base_url = 'http://download.tensorflow.org/models/object_detection/'
+#   model_file = model_name + '.tar.gz'
+#   model_dir = tf.keras.utils.get_file(
+#     fname=model_name, 
+#     origin=base_url + model_file,
+#     untar=True)
+
+#   model_dir = pathlib.Path(model_dir)/"saved_model"
+
+#   model = tf.saved_model.load(str(model_dir))
+#   model = model.signatures['serving_default']
+
+#   return model
+
+
+def load_graph(model_file):
+  graph = tf.Graph()
+  graph_def = tf.GraphDef()
+
+  with open(model_file, "rb") as f:
+    graph_def.ParseFromString(f.read())
+  with graph.as_default():
+    tf.import_graph_def(graph_def)
+  return graph
+
+def load_labels(label_file):
+    label = []
+    proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+    for l in proto_as_ascii_lines:
+        label.append(l.rstrip())
+    return label
+
+def run_model(img, graph):
+    image_reader = tf.convert_to_tensor(img, np.float32)
+    #float_caster = tf.cast(image_reader, tf.float32)
+    dims_expander = tf.expand_dims(image_reader, 0)
+    # resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+    # normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+    sess = tf.Session()
+    result = sess.run([image_reader])
+    with tf.Session(graph=graph) as sess:
+        start = time.time()
+        input_layer = "input"
+        output_layer = "final_result"
+        input_name = "import/" + input_layer
+        output_name = "import/" + output_layer
+        input_operation = graph.get_operation_by_name(input_name)
+        output_operation = graph.get_operation_by_name(output_name)
+        results = sess.run(output_operation.outputs[0],
+                        {input_operation.outputs[0]: result})
+        end=time.time()
+    results = np.squeeze(results)
+
+    # label_file = '/home/Techno-Disaster/tensorflow-for-poets-2/tf_files/retrained_labels.txt'
+    top_k = results.argsort()[-5:][::-1]
+    # labels = load_labels(label_file)
+
+    labels = ['chapati','dal','rice','veggies']
+
+    print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
+    template = "{} (score={:0.5f})"
+    for i in top_k:
+        print(template.format(labels[i], results[i]))
+
+# Define the video stream
+cap = cv2.VideoCapture(0)  # Change only if you have more than one webcams
+# cap.set(3,224)
+# cap.set(4,224)
+graph = load_graph('/home/Techno-Disaster/tensorflow-for-poets-2/tf_files/retrained_graph.pb')
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (224, 224))
+    run_model(frame, graph)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+# What model to download.
+# Models can bee found here: https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md
+MODEL_NAME = 'mobilenet_v1_0.50_224_frozen.tgz'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = '/home/Techno-Disaster/tensorflow-for-poets-2/tf_files/retrained_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = '/home/Techno-Disaster/tensorflow-for-poets-2/tf_files/retrained_labels.txt'
+
+# Number of classes to detect
+NUM_CLASSES = 90
+
+# Download Model
+# opener = urllib.request.URLopener()
+# opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+# tar_file = tarfile.open(MODEL_FILE)
+# for file in tar_file.getmembers():
+#     file_name = os.path.basename(file.name)
+#     if 'frozen_inference_graph.pb' in file_name:
+#         tar_file.extract(file, os.getcwd())
+
+
+# Load a (frozen) Tensorflow model into memory.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+
+# Loading label map
+# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(
+    label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+# Helper code
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
+
+
+# Detection
+with detection_graph.as_default():
+    with tf.Session(graph=detection_graph) as sess:
+        while True:
+
+            # Read frame from camera
+            ret, image_np = cap.read()
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+            # Extract image tensor
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            # Extract detection boxes
+            boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            # Extract detection scores
+            scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            # Extract detection classes
+            classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            # Extract number of detectionsd
+            num_detections = detection_graph.get_tensor_by_name(
+                'num_detections:0')
+            # Actual detection.
+            (boxes, scores, classes, num_detections) = sess.run(
+                [boxes, scores, classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+            # Visualization of the results of a detection.
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                image_np,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=8)
+
+            # Display output
+            cv2.imshow('object detection', cv2.resize(image_np, (800, 600)))
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
